9db3b9e6aa91a88316e9678480f3faa48f1ac2a6
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2021-04-07 13:10:17 +0400

    Learn 30. Done.

--- js/script.js
+++ js/script.js
@@ -24,3 +24,23 @@ const movieDB = {
     ]
 };
 
+// 1)
+const promoAdv = document.getElementsByClassName('promo__adv'),
+    promoContent = document.getElementsByClassName('promo__content');
+
+promoAdv[0].remove();
+promoContent[0].style.cssText = 'width:calc(100% - 300px)';
+
+// 2)
+promoContent[0].getElementsByClassName('promo__genre')[0].textContent = 'ДРАМА';
+
+// 3)
+promoContent[0].getElementsByClassName('promo__bg')[0].style.cssText = 'background:url("../img/bg.jpg"';
+// promoContent[0].style.cssText = 'promo__bg{height:360px;background:url("../img/bg.jpg")'
+
+// 4-5)
+const iList = promoContent[0].getElementsByClassName('promo__interactive-list')[0].getElementsByClassName('promo__interactive-item');
+
+movieDB.movies.sort().forEach((element, index) => {
+    iList[index].textContent = `${index + 1}. ${element}`
+});
